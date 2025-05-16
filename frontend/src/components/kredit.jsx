e722b95ec72e21f38324cfdd9031c58d01a3d1ce
briskaanandaa <briskaananda103@gmail.com>
2025-05-16 22:31:55 +0700

    kredit

--- frontend/src/components/kredit.jsx
+++ frontend/src/components/kredit.jsx
@@ -10,7 +10,7 @@ const Credit = () => {
 
   return (
     <div
-      className="fixed bottom-4 right-4 text-sm text-gray-500 cursor-pointer "
+      className="fixed bottom-4 right-4 text-sm text-background px-4 py-2 cursor-pointer bg-primary rounded-full"
       onClick={handleClick}
     >
       <span>Made with React by Briska Ananda</span>
